fc4736750f9a57d25cb7f8c0ce830fe1e2d4c992
Marlon Braga <rdg.brg@gmail.com>
2020-04-22 23:26:01 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,10 +2,8 @@ current_time = Time.now.min
 
 isItEvenOdd = current_time.to_i % 2 === 0 
 
-puts isItEvenOdd
-
-# if 
-#   puts "Even"
-# else
-#   puts "Odd"
-# end
+if isItEvenOdd
+  puts "Even"
+else
+  puts "Odd"
+end
